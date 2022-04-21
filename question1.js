f2c7ff952a75d94e748cbfac48daaf0618648dff
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2022-04-21 19:10:04 +0530

    Add files via upload

---
+++ question1.js
@@ -0,0 +1,28 @@
+// QUESTION 1
+
+const arr=[1,1,2,4,4,4,4]
+function migratoryBirds(arr) {
+    let objectArr=arr.reduce((obj, b)=> {
+        obj[b] = ++obj[b] || 1;
+        return obj;
+      }, {});
+//      console.log(objectArr)
+     const maxVal=Math.max(...Object.values(objectArr))
+     const num=Object.keys(objectArr).find(key => objectArr[key] === maxVal)
+   
+
+return num
+}
+
+console.log(migratoryBirds(arr))
+    
+
+
+
+
+
+
+
+
+
+
